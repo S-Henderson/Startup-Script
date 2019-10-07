137dcda062fdc15014363369754ea83e5ec8e506
S-Hend12 <52982548+S-Hend12@users.noreply.github.com>
2019-10-06 21:22:40 -0400

    Create Startup_Script.py

---
+++ Startup_Script.py
@@ -0,0 +1,11 @@
+import webbrowser
+
+#list of websites to open
+url_1 = "https://www.google.ca"
+url_2 = "https://www.youtube.com"
+url_3 = "https://login.360incentives.com/360Core/Login.aspx"
+
+#opens websites
+webbrowser.open_new(url_1)
+webbrowser.open_new(url_2)
+webbrowser.open_new(url_3)
