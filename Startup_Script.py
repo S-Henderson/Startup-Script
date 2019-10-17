b4d3b76b5f9af6272e4bb7f62f425daad1dba487
S-Hend12 <52982548+S-Hend12@users.noreply.github.com>
2019-10-17 15:16:36 -0400

    Update Startup_Script.py

--- Startup_Script.py
+++ Startup_Script.py
@@ -1,9 +1,18 @@
+import os
 import webbrowser
 
+#list of apps to open
+app_1 = "outlook.exe"
+app_2 = "slack.exe"
+
 #list of websites to open
-url_1 = "https://www.google.ca"
-url_2 = "https://www.youtube.com"
-url_3 = "https://login.360incentives.com/360Core/Login.aspx"
+url_1 = "https://login.360incentives.com/360Core/Login.aspx"
+url_2 = "https://360in.sharepoint.com/Operations/POS/Forms/AllItems.aspx"
+url_3 = "https://360insights.atlassian.net/projects/POS/queues/custom/243"
+
+#opens apps
+os.startfile(app_1)
+os.startfile(app_2)
 
 #opens websites
 webbrowser.open_new(url_1)
