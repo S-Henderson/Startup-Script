beab069cbe948a6b2a5759dbdc757b9ecee9086a
S-Hend12 <52982548+S-Hend12@users.noreply.github.com>
2019-11-07 17:34:38 -0500

    Add files via upload

---
+++ Startup_Script_v1.py
@@ -0,0 +1,30 @@
+import os
+import webbrowser
+
+desktopPath = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+
+#list of apps to open
+app_1 = "outlook.exe"
+app_2 = "slack.exe"
+app_3 = desktopPath + "\Data Management_V5.5.accdb"
+
+#list of websites to open
+url_1 = "https://www.youtube.com"
+url_2 = "https://360in.sharepoint.com/Operations/POS/Forms/AllItems.aspx"
+url_3 = "https://360insights.atlassian.net/projects/POS/queues/custom/243"
+url_4 = "https://login.360incentives.com/360Core/Login.aspx"
+
+print("Opening Stuff...")
+
+#opens apps
+os.startfile(app_1)
+os.startfile(app_2)
+os.startfile(app_3)
+
+#opens websites
+webbrowser.open(url_1)
+webbrowser.open_new_tab(url_2)
+webbrowser.open_new_tab(url_3)
+webbrowser.open_new_tab(url_4)
+
+print("Script Completed")
