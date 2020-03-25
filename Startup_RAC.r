e8bbb30bacce57c0f20e1a90128e36ef7a27d831
S-Hend12 <52982548+S-Hend12@users.noreply.github.com>
2020-03-25 12:20:48 -0400

    Update Startup_RAC.r

--- Startup_RAC.r
+++ Startup_RAC.r
@@ -1,15 +1,23 @@
-#list of websites to open
+#--------------- SETUP ---------------
+
+# list of websites to open
 url_1 = "https://login.360incentives.com/360Core/Login.aspx"
 url_2 = "https://drive.google.com/drive/folders/0B-VKTqswzwr3MUt3ekJvaHBfb0E"
 url_3 = "http://360incentives.freshdesk.com/en/support/login"
 url_4 = "https://360insights.atlassian.net/wiki/spaces/CC/pages/806092938/Risk+and+Compliance+RAC"
 
+#--------------- ACTION ---------------
+
+# opens websites - uses default browser
+open_websites <- function() {
+  browseURL(url_1)
+  browseURL(url_2)
+  browseURL(url_3)
+  browseURL(url_4)
+}
+
 print("Opening Stuff...")
 
-#opens websites - uses default browser
-browseURL(url_1)
-browseURL(url_2)
-browseURL(url_3)
-browseURL(url_4)
+open_websites()
 
 print("Script Completed")
